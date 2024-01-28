3fad54cf31ef2d7c6ce8485e6c57d9cbd83c79ea
Hasan <124549931+HasanJunior@users.noreply.github.com>
2024-01-28 21:33:45 +0500

    Create oop1.py
    OOP ga kirish

---
+++ oop1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 21:24:52 2024
+
+@author: user
+"""
+
+# OOP 1
+
+class Talaba:
+    def __init__(self, ism, fam, tyil):
+        self.ism = ism
+        self.fam = fam
+        self.tyil = tyil
+
+talaba1 = Talaba("Hasan", "Turdimurotov", 2002)
+
+print(talaba1.fam, talaba1.ism)
